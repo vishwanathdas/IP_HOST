594abf50f24fa92f17b2a469d8dca652e05a4c0d
Biharwalacoder <111034515+vishwanathdas@users.noreply.github.com>
2024-01-04 17:12:40 +0530

    IP_HOST.py

---
+++ IP_HOST.py
@@ -0,0 +1,27 @@
+import socket
+from tabulate import tabulate
+
+class IpHost(object):
+    def __init__(self, url="www.google.com"):
+        self.url = url
+        self.host_name = socket.gethostname()
+        self.ip_address = socket.gethostbyname(self.host_name)
+        self.remote_ip = self.remote_info()
+
+    def __repr__(self):
+        data = {
+            "Host Name": [self.host_name],
+            "IP Address": [self.ip_address],
+            f"{self.url}": [self.remote_ip]
+        }
+        table = tabulate(data, headers="keys", tablefmt="pretty")
+        return table
+
+    def remote_info(self):
+        try:
+            return socket.gethostbyname(self.url)
+        except socket.error as errmsg:
+            return errmsg
+
+if __name__ == "__main__":
+    print(IpHost())
